17245226a268a01273e960dd37fca5926c68437b
jfjia <762605185@qq.com>
2023-04-09 10:41:28 +0800

    build with latest msys2 version

--- build_mingw.sh
+++ build_mingw.sh
@@ -5,8 +5,8 @@ if [ ! -d "build_mingw" ]; then
     mkdir build_mingw
 fi
 cd build_mingw
-cmake -DWITH_EXAMPLES=ON -G "MSYS Makefiles" ..
-make clean
-make -j2 >1.log 2>2.log
+cmake -DWITH_EXAMPLES=ON -DCMAKE_BUILD_TYPE=Release -G "MinGW Makefiles" ..
+mingw32-make clean
+mingw32-make -j2 >1.log 2>2.log
 cat 2.log
 cd ..
